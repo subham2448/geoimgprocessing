2e410c4ee3fd2a11c2bdacb92f49cf08b0a198d7
subham <subham.chowdhury.bksc@ggmail.com>
2021-06-18 17:01:12 +0530

    bounding box search implemented

--- Program.cs
+++ Program.cs
@@ -26,13 +26,17 @@ namespace firstcapp
                 using var httpClient = new HttpClient();
 
                 // input datas
-                var bdbx = "45.219,-122.325,47.610,-122.107";
-                var viewType = "Aerial";
-                var zoom = 14;
+                // var bdbx = "47.611833900404896,  -122.36456394195557,  47.626268662877358,  -122.34314918518068";
+                var bdbx = "62.443528,  -114.440968, 62.453119, -114.407630";
+                // var bdbx = "4761.9048,-122.35384";
+                var viewType = "AerialWithLabels";
+               
 
 
                 // API to be used
-                var url = $"https://dev.virtualearth.net/REST/V1/Imagery/Map/{viewType}/{bdbx}/8?zoomLevel={zoom}&mapSize=2000,1500&format=jpeg&key=AvIGA0FCQHHBF0k_ACLe2v-gxTbnT2vQhqR1mS303f1jJ4JLpFERujV0Awrd9gQb";
+                // var url = $"https://dev.virtualearth.net/REST/V1/Imagery/Map/{viewType}/{bdbx}/8?dpi=Large&zoomLevel={zoom}&mapSize=2000,1500&format=jpeg&key=AvIGA0FCQHHBF0k_ACLe2v-gxTbnT2vQhqR1mS303f1jJ4JLpFERujV0Awrd9gQb";
+                var url = $"https://dev.virtualearth.net/REST/V1/Imagery/Map/{viewType}?ma={bdbx}&dpi=Large&mapSize=2000,1500&format=jpeg&key=AvIGA0FCQHHBF0k_ACLe2v-gxTbnT2vQhqR1mS303f1jJ4JLpFERujV0Awrd9gQb";
+
                 var uri = new Uri(url);
 
                 //directory path
@@ -46,7 +50,7 @@ namespace firstcapp
 
                 // Get the file extension
                 var uriWithoutQuery = uri.GetLeftPart(UriPartial.Path);
-                var fileExtension = Path.GetExtension(uriWithoutQuery);
+                // var fileExtension = Path.GetExtension(uriWithoutQuery);
 
                 // Create file path and ensure directory exists
                 var path = Path.Combine(directoryPath, $"{fileName}");
